8cebfb771e4d26a20ea3eff008800efda17c2b48
djjoel12 <familledan12@gmail.com>
2026-08-21 07:21:16 +0000

    Update TileLayer URL for OpenStreetMap

--- src/components/map/TransportMap.tsx
+++ src/components/map/TransportMap.tsx
@@ -133,9 +133,9 @@ export default function TransportMap({
         ref={mapRef}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-        />
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+/>
 
         {routePositions.length > 1 && (
           <>
