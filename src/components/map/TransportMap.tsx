3c4bde6e11eff6a7b57157fe78542d4ae9225a7a
djjoel12 <familledan12@gmail.com>
2026-08-21 07:16:50 +0000

    Change TileLayer URL to CartoDB light basemap

--- src/components/map/TransportMap.tsx
+++ src/components/map/TransportMap.tsx
@@ -134,7 +134,7 @@ export default function TransportMap({
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
 
         {routePositions.length > 1 && (
