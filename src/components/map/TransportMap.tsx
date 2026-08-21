ff4506ac00cfa97980a4ae9df9d482fb16007a45
djjoel12 <familledan12@gmail.com>
2026-08-21 21:43:14 +0000

    Update TransportMap.tsx

--- src/components/map/TransportMap.tsx
+++ src/components/map/TransportMap.tsx
@@ -134,7 +134,7 @@ export default function TransportMap({
       >
         <TileLayer
   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
 />
 
         {routePositions.length > 1 && (
